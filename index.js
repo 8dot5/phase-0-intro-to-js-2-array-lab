4ed5460d8aeb8832042d9e60e7aa9ea4075ca9e3
Chun Yi <8.5by11@gmail.com>
2021-12-27 17:35:24 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
 // Write your solution here!
+
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+};
+
+destructivelyAppendCat;
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+};
+
+destructivelyPrependCat;
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+};
+
+destructivelyRemoveLastCat;
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+};
+
+destructivelyRemoveFirstCat;
+
+
+
+const copyOfCats = [...cats];
+
+function appendCat() {
+    copyOfCats.push("Broom");
+    return copyOfCats;
+}
+
+appendCat;
+
+const anotherCopyOfCats = [...cats];
+
+function prependCat() {
+    anotherCopyOfCats.unshift("Arnold");
+    return anotherCopyOfCats;
+}
+
+prependCat;
+
+const triceCopyOfCats = [...cats];
+
+function removeLastCat() {
+    triceCopyOfCats.pop();
+    return triceCopyOfCats;
+}
+
+removeLastCat;
+
+const quadCopyOfCats = [...cats];
+
+function removeFirstCat() {
+    quadCopyOfCats.shift();
+    return quadCopyOfCats;
+}
+
+removeFirstCat;
